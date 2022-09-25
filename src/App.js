7e5c9d887671f523347a7dff626d667f3a61c78d
shimpei hashizume <shinpei.hashizume714@gmail.com>
2022-09-25 15:06:30 +0900

    [updata]ローディング追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAllPokemon } from './untils/pokemon';
 
 
 function App() {
   const initializeURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initializeURL);
       console.log(res);
+      setLoading(false);
     }
     fetchPokemonData();
   }, []);
 
   return (
     <div className="App" >
-
+      {loading ? (
+        <h1>ローディング中...</h1>
+      ) : (
+          <>
+            <h1>ポケモンデータを取得しました</h1>
+          </>
+        )}
     </div>
   );
 }
