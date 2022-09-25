c2db8904708d1daa7f67bbb4666b1ad39a93557b
shimpei hashizume <shinpei.hashizume714@gmail.com>
2022-09-25 15:45:48 +0900

    [update]ポケモンの詳細データを取得

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { getAllPokemon } from './untils/pokemon';
+import { getAllPokemon, getPokemon } from './untils/pokemon';
 
 
 function App() {
   const initializeURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initializeURL);
-      console.log(res);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
       setLoading(false);
     }
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    // 複数のPromise関数を実行し、全ての結果を得る
+    let _pokemonData = await Promise.all(
+      // ポケモンデータを配列に変換
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
 
   return (
     <div className="App" >
